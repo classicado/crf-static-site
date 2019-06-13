265233300c6a6bc3524c9f8e6d872f3cdb29e7a0
Adolf Mapadimeng <classicado@gmail.com>
2019-06-13 14:54:18 +0200

    Updated dynamic rates

--- dynamic-rates.php
+++ dynamic-rates.php
@@ -19,51 +19,63 @@
  <br>
  <br>
 
-  <h2> Our dynamic charge models is simalar to those of Uber and Taxify</h2>
+  <h3> Our dynamic charge models is simalar to those of Uber and Taxify but much more affordable</h3>
+ <br>
  
- 
+<div class="row">
+  <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+     <style type="text/css">
+    .tg  {border-collapse:collapse;border-spacing:0;border-color:#93a1a1;}
+    .tg td{font-family:Arial, sans-serif;font-size:14px;padding:10px 5px;border-style:solid;border-width:0px;overflow:hidden;word-break:normal;border-color:#93a1a1;color:#002b36;background-color:#fdf6e3;}
+    .tg th{font-family:Arial, sans-serif;font-size:14px;font-weight:normal;padding:10px 5px;border-style:solid;border-width:0px;overflow:hidden;word-break:normal;border-color:#93a1a1;color:#fdf6e3;background-color:#657b83;}
+    .tg .tg-0pky{border-color:inherit;text-align:left;vertical-align:top}
+    .tg .tg-0lax{text-align:left;vertical-align:top}
+    </style>
+    <table class="tg">
+      <tr>
+        <th class="tg-0pky">Fees</th>
+        <th class="tg-0pky">Comfort Rides<br> Fleet</th>
+        <th class="tg-0pky">Taxify</th>
+        <th class="tg-0pky">UberX</th> 
+      </tr>
+      <tr>
+        <td class="tg-0lax">Base Fee</td>
+        <td class="tg-0lax">R5.00</td>
+        <td class="tg-0lax">R5.00</td>
+        <td class="tg-0lax">R5.00</td> 
+      </tr>
+      <tr>
+        <td class="tg-0lax">Cost per km</td>
+        <td class="tg-0lax">R6.00</td>
+        <td class="tg-0lax">R6.50</td>
+        <td class="tg-0lax">R7.00</td>
+      </tr>
+      <tr>
+        <td class="tg-0lax">Cost per minute</td>
+        <td class="tg-0lax">R0.00</td>
+        <td class="tg-0lax">R0.70</td>
+        <td class="tg-0lax">R0.70</td>
+      </tr>
+      <tr>
+        <td class="tg-0lax">Minimum fare</td>
+        <td class="tg-0lax">R20.00</td>
+        <td class="tg-0lax">R20.00</td>
+        <td class="tg-0lax">R20.00</td>
+      </tr>
+    </table>
+    
+  </div>
+  <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6"> 
+    <p> Important differences to note : 
+      <br>-we do not have  Surge pricing. 
+      <br>-We do no charge per minute 
+      <br>-We do not have cancellations Fees 
+    </p>    
+  </div>  
+</div>
 
- <style type="text/css">
-.tg  {border-collapse:collapse;border-spacing:0;border-color:#93a1a1;}
-.tg td{font-family:Arial, sans-serif;font-size:14px;padding:10px 5px;border-style:solid;border-width:0px;overflow:hidden;word-break:normal;border-color:#93a1a1;color:#002b36;background-color:#fdf6e3;}
-.tg th{font-family:Arial, sans-serif;font-size:14px;font-weight:normal;padding:10px 5px;border-style:solid;border-width:0px;overflow:hidden;word-break:normal;border-color:#93a1a1;color:#fdf6e3;background-color:#657b83;}
-.tg .tg-0pky{border-color:inherit;text-align:left;vertical-align:top}
-.tg .tg-0lax{text-align:left;vertical-align:top}
-</style>
-<table class="tg">
-  <tr>
-    <th class="tg-0pky">Fees</th>
-    <th class="tg-0pky">Comfort Rides<br> Fleet</th>
-    <th class="tg-0pky">Taxify</th>
-    <th class="tg-0pky">UberX</th> 
-  </tr>
-  <tr>
-    <td class="tg-0lax">Base Fee</td>
-    <td class="tg-0lax">R5.00</td>
-    <td class="tg-0lax">R5.00</td>
-    <td class="tg-0lax">R5.00</td> 
-  </tr>
-  <tr>
-    <td class="tg-0lax">Cost per km</td>
-    <td class="tg-0lax">R6.00</td>
-    <td class="tg-0lax">R6.50</td>
-    <td class="tg-0lax">R7.00</td>
-  </tr>
-  <tr>
-    <td class="tg-0lax">Cost per minute</td>
-    <td class="tg-0lax">R0.00</td>
-    <td class="tg-0lax">R0.70</td>
-    <td class="tg-0lax">R0.70</td>
-  </tr>
-  <tr>
-    <td class="tg-0lax">Minimum fare</td>
-    <td class="tg-0lax">R20.00</td>
-    <td class="tg-0lax">R20.00</td>
-    <td class="tg-0lax">R20.00</td>
-  </tr>
-</table>
-  
-  <p> Another important difference to note is that we do not have  Surge pricing. This means that our dynamic rates do not change. </p>
+
+
  
 </div>
 
